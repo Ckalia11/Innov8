b8bd144fdc48e2e5f2226e3f216c8e000c4b394d
Chetan Kalia <ckalia@edu.uwaterloo.ca>
2019-11-18 12:14:01 -0500

    first commit

---
+++ startbootstrap-sb-admin-gh-pages/index.js
@@ -0,0 +1,27 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work',     11],
+    ['Eat',      2],
+    ['Commute',  2],
+    ['Watch TV', 2],
+    ['Sleep',    7]
+  ]);
+
+  var options = {
+    title: 'My Daily Activities'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+  
+   google.visualization.events.addListener(chart, 'select', function() {
+var row = chart.getSelection()[0].row;
+window.open('http://www.google.com'); //This will open Google in a new window.
+});
+}
